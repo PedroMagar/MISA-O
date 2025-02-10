d4977a5b05395460a75b1981268196ce3fb25100
Pedro H M Garcia <pedro.phmg@gmail.com>
2025-02-10 12:38:28 -0300

    Test procedures

---
+++ scripts/misa-o_b.sh
@@ -0,0 +1,3 @@
+cd build
+iverilog -g2012 -Wall -o ../run/tb_misao.vvp ../design/misa-o_ref.sv ../testbench/misa-o_global.tb
+vvp ../run/tb_misao.vvp
---
+++ scripts/misa-o_r.sh
@@ -0,0 +1,4 @@
+cd build
+iverilog -g2012 -Wall -o ../run/tb_misao.vvp ../design/misa-o_ref.sv ../testbench/misa-o_global.tb
+vvp ../run/tb_misao.vvp
+gtkwave ../run/output.vcd
